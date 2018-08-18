51db1998beed687068c721b1b88f890ed9207814
Aayushi <adangol1@swarthmore.edu>
2018-08-18 22:40:23 +0545

    blurpee added

---
+++ Assets/blink.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blink : MonoBehaviour {
+
+	Color _orig;
+	Material _mat;
+	public Color start;
+	public Color end;
+	public float rate = 5;
+	public Color lerpedColor;
+
+	// Use this for initialization
+	void Start () {
+		Renderer[] renderers = GetComponentsInChildren<Renderer> ();
+		Material m = renderers [0].material;
+		_orig = m.color;
+		_mat = m;
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			lerpedColor = Color.Lerp (end, start, Mathf.PingPong (rate * Time.time, 1));
+			_mat.color = lerpedColor;
+
+	}
+
+}
---
+++ Assets/rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+	public float rate = 10;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("position = " + transform.position);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.AngleAxis((Time.realtimeSinceStartup * rate), Vector3.up);
+		
+	}
+}
